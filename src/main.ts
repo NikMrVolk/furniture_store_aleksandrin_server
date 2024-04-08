937a0d712ee907efbb3ff95330fde3313f223292
NickZavadski <nick.mr.volk@gmail.com>
2024-04-08 05:32:22 +0300

    chore: move cookie parser

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,12 @@ const start = async () => {
         const app = await NestFactory.create(AppModule)
 
         app.setGlobalPrefix('api')
+        app.use(cookieParser())
         app.enableCors({
             origin: process.env.CLIENT_URL,
             credentials: true,
             exposedHeaders: 'set-cookie',
         })
-        app.use(cookieParser())
 
         await app.listen(PORT, () =>
             console.log(`App started work on port ${PORT}`),
